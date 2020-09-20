760e7fe8c7d4c1020687d992299f40f0237df29c
Jaganmohan <jaganmohan@MacBook-Pro.local>
2020-09-20 17:48:33 +0530

    Linked list updted

---
+++ src/linked_list/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+package linked_list;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        Node node = Node.getLinkedList(false);
+        Node.printLinkedList(reverseList(node));
+    }
+
+    private static Node reverseList(Node node) {
+        Node prevNode = null , currentNode = node, next;
+        while (currentNode!=null){
+            next = currentNode.next; 
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = next;
+        }
+        return prevNode;
+    }
+}
